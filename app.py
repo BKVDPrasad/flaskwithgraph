dfd2492fe04b1b4b1b75a2dd94f62d7ecef1b463
BKVDPrasad <54040086+BKVDPrasad@users.noreply.github.com>
2020-07-23 11:46:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ import unicodedata
 # from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///test.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db_URI = os.environ.get('DATABASE_URL', 'sqlite:///test.db')
